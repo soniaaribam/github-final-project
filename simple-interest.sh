8d2780c1aad895f10802b05ff477e1bebc8a2bea
soniaaribam <51089319+soniaaribam@users.noreply.github.com>
2025-12-17 00:04:53 +0530

    simple-interest.md
    Updated the script to include README content and removed the original script details.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,28 +1,10 @@
-   #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+  # This is the README.md file for the **github-final-project**
 
-   # Do not use this in production. Sample purpose only.
+A calculator that calculates simple interest given principal, annual rate of interest and time period in years.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <soniaaribam>
-
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
-
-   # Output:
-   # simple interest = p*t*r
-
-   echo "Enter the principal:"
-   read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
-
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+Input:
+   p, principal amount
+   t, time period in years
+   r, annual rate of interest
+Output
+   simple interest = p*t*r
